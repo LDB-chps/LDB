6889e8e9b3a5cf19d9ae0f071f5261277455e973
JAM Mathys <mathys.jam@ens.uvsq.fr>
2022-02-08 10:56:42 +0100

    Implemented Process methods

--- include/tracing/process.h
+++ include/tracing/process.h
@@ -3,6 +3,10 @@
 #include <memory>
 
 namespace ldb {
+
+  /**
+   * @brief Lightweight non-copyable process handle
+   */
   class Process {
   public:
     /**
@@ -13,6 +17,19 @@ namespace ldb {
     explicit Process(pid_t pid);
 
     /**
+     * @brief Process Handle are not copyable
+     */
+    Process(const Process&) = delete;
+
+    /**
+     * @brief Process Handle are not copyable
+     */
+    Process& operator=(const Process&) = delete;
+
+    Process(Process&&) = default;
+    Process& operator=(Process&&) = default;
+
+    /**
      * @brief Launch the command with its argument in a new process and return a Process handle to
      * it. The new process will automatically call ptrace() to attach to itself to the parent
      * process.
@@ -21,8 +38,7 @@ namespace ldb {
      * @param args
      * @return
      */
-    static std::unique_ptr<Process> fromCommand(const std::string& command,
-                                                const std::string& args);
+    static Process fromCommand(const std::string& command, const std::string& args);
 
     /**
      * @brief Signal the process to resume execution.
@@ -46,7 +62,9 @@ namespace ldb {
      * @brief Get the pid of the process. Does not mean the process is running
      * @return The pid of the process
      */
-    pid_t getPid();
+    pid_t getPid() {
+      return pid;
+    }
 
     /**
      * @brief Check if the process is running
@@ -57,7 +75,7 @@ namespace ldb {
     /**
      * @brief Wait for the process to exit or for a signal to raise.
      */
-    void waitFor();
+    void wait();
 
   private:
     pid_t pid = 0;
--- src/tracing/process.cpp
+++ src/tracing/process.cpp
@@ -1,4 +1,49 @@
 #include "process.h"
+#include <csignal>
+#include <iostream>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+namespace ldb {
 
 
-namespace ldb {}
+  Process::Process(pid_t pid) : pid(pid) {}
+
+  Process Process::fromCommand(const std::string& command, const std::string& args) {
+    Process res(fork());
+    if (res.pid == 0) {
+      ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
+      // Since we switch to a new process, no need to worry about this object state anymore
+      execlp(command.c_str(), command.c_str(), args.c_str(), nullptr);
+      exit(1);
+    }
+    return res;
+  }
+
+  bool Process::resume() {
+    return ptrace(PTRACE_CONT, pid, nullptr, nullptr) == 0;
+  }
+
+  bool Process::pause() {
+    return ::kill(pid, SIGSTOP) == 0;
+  }
+
+  bool Process::kill() {
+    return ptrace(PTRACE_KILL, pid, nullptr, nullptr) == 0;
+  }
+
+  bool Process::isAlive() {
+    // The process may be waiting for us to sync up, so we try to wait for it
+    waitpid(pid, nullptr, WNOHANG);
+
+    // Send a kill signal and check errno for the "no such process" error
+    if (::kill(pid, 0) == -1 and errno != ESRCH) { return false; }
+    return true;
+  }
+
+  void Process::wait() {
+    waitpid(pid, nullptr, 0);
+  }
+}// namespace ldb
