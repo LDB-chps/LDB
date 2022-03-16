ca81ad8db0801811f99c18e6429305e9bb50e3a5
johnkyky <ugobattiston@gmail.com>
2022-03-16 01:16:13 +0100

    implémentation de la fonction addr2line pour transformer une adresse en une ligne dans le fichier source

--- apps/readDebugInfo.cpp
+++ apps/readDebugInfo.cpp
@@ -4,15 +4,23 @@
 #include "DwarfReader.h"
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " <executable> <pid>" << std::endl;
-    return 1;
-  }
+  // if (argc != 3) {
+  //   std::cerr << "Usage: " << argv[0] << " <executable> <pid>" << std::endl;
+  //   return 1;
+  // }
 
-  ldb::Process p(std::atoi(argv[2]));
-  auto infos = ldb::readDebugInfo(argv[1], p);
-  if (not infos) return 1;
-  std::cout << *infos->getSymbolTable() << std::endl;
+  // ldb::Process p(std::atoi(argv[2]));
+  // auto infos = ldb::readDebugInfo(argv[1], p);
+  // if (not infos) return 1;
+  // std::cout << *infos->getSymbolTable() << std::endl;
+
+  auto res = ldb::addr2Line(argv[1], std::atoi(argv[2]));
+
+  if (res) {
+    std::cout << "file: " << res->first << "\nline: " << res->second << std::endl;
+  } else {
+    std::cerr << "addr or file don't work" << std::endl;
+  }
 
   return 0;
 }
--- include/tracing/ELFParser.h
+++ include/tracing/ELFParser.h
@@ -5,10 +5,17 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <filesystem>
+#include <libelf.h>
+#include <utility>
+#include <optional>
+#include <sstream>
 
 namespace ldb {
 
   // We won't be wrapping libelf or libdwarf, so we'll keep the code simple without using classes
   std::unique_ptr<const DebugInfo> readDebugInfo(const std::filesystem::path& filePath, Process& S);
+
+  std::optional<std::pair<std::string, size_t>> addr2Line(const std::filesystem::path& path, const Elf64_Addr addr);
 
 }// namespace ldb
